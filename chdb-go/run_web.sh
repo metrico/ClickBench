cdc113eab6acefe9649e90672b6630aa85cefea0
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2023-04-30 10:49:28 +0200

    testing chdb-go

---
+++ chdb-go/run_web.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+TRIES=3
+QUERY_NUM=1
+cat queries_web.sql | while read query; do
+    sync
+    echo 3 | sudo tee /proc/sys/vm/drop_caches >/dev/null
+    echo -n "["
+    for i in $(seq 1 $TRIES); do
+        RES=$(curl -w 'Time: %{time_total}\n' "https://default@chdb.fly.io" -d "${query}" 2>&1 | grep -P '^Time: ' | sed 's/Time: //')
+        [[ "$?" == "0" ]] && echo -n "${RES}" || echo -n "null"
+        [[ "$i" != $TRIES ]] && echo -n ", "
+
+        echo "${QUERY_NUM},${i},${RES}" >> result.csv
+    done
+    echo "],"
+
+    QUERY_NUM=$((QUERY_NUM + 1))
+
+
+    # ./query.py <<< "${query}"
+done
